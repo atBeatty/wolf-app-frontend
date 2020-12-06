5ed610d991d529c752ed270f84dcb18fc3c6e7b0
atBeatty <adam.t.beatt@gmail.com>
2020-12-06 14:27:36 -0800

    sucessfully got HTML rendered from backend

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector("body").innerText = "WHATS UP EVERYONE"
     console.log('DOM fully loaded and parsed');
+    fetchCourses()
 });
+
+
+
+function fetchCourses(){
+    fetch("http://localhost:3000/courses")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+
+    });
+}
+
+// ("http://localhost:3000/courses")
+
+function displayCourses(courseObject){
+    const bodyElement = document.querySelector("body")
+    bodyElement.innerText += `${courseObject}`
+    
+}
+
+displayCourses()
