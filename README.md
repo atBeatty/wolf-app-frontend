a30e529efaec5e346939479304bfc7efb01bbdb5
Adam <adam.t.beatt@gmail.com>
2021-04-27 13:43:41 -0700

    update readme

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-# wolf-app-frontend
-
-
+# WOLF-FRONTEND
  
-* front end code
-* [Back end here](https://github.com/atbeatty/wolf-app-backend)
+
+(https://github.com/atbeatty/wolf-app-backend)
 
 ### Instructions
-1. Clone repo to local machine.
+1. Make sure that both of the repositories are cloned to your local machine.
 
-1. Navigate to the filepath of the *backend* repository in the command line
-1. Start the rails server
-1. Navigate to the filepath of the *frontend* and open up the index.html file from the root
+**Backend First**
+1. Open a terminal and navigate to the wolf-app-backend directory.
+1. Type in the following to the command line.
+
+```bash
+bundle install
+```
+
 
 ### MVP
 This project is designed with future developments in mind. The long term plan is to develop this is into an app that can allow the user to choose from a variety of golf games, and have it track gambling totals or points. 
--- services/API.js
+++ services/API.js
@@ -1,7 +1,7 @@
 
 class API {
 
-    static updateScores(gameId, hole, scores){
+    static updateScores(gameId, hole, scores) {
         fetch(`http://localhost:3000/wolf_games/${gameId}`, {
             method: "PATCH",
             headers: {
@@ -13,17 +13,17 @@ class API {
                 scores: `${scores}`,
                 wolfGameId: `${gameId}`
             })
-        }) 
-        .then(resp => resp.json())
-        .then(json => {
-            const id = json.id
-            const foursome_id = json.foursome[0].foursome_id
-            const stakes = json.stakes
-            const course_id = json.holes[0].course_id
-            const newGameObj = new WolfGame(id, stakes, foursome_id, course_id)
-            // console.log("updated then return this", newGameObj)
-            newGameObj.renderWithUpdatedScores(hole)
-            // document.querySelector(".stakes-container").innerHTML += `${json.holes[0].score}`
         })
+            .then(resp => resp.json())
+            .then(json => {
+                const id = json.id
+                const foursome_id = json.foursome[0].foursome_id
+                const stakes = json.stakes
+                const course_id = json.holes[0].course_id
+                const newGameObj = new WolfGame(id, stakes, foursome_id, course_id)
+                // console.log("updated then return this", newGameObj)
+                newGameObj.renderWithUpdatedScores(hole)
+                // document.querySelector(".stakes-container").innerHTML += `${json.holes[0].score}`
+            })
     }
 }
